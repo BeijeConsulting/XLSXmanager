b5db60a6d38f782692cc4ece873acaa16bae9b0f
matteo provezza <matteoprovezza98@gmail.com>
2022-08-02 14:52:48 +0200

    xlsx manager main

--- src/main/java/it/beije/xlsxmanager/util/XLSXManager.java
+++ src/main/java/it/beije/xlsxmanager/util/XLSXManager.java
@@ -6,15 +6,74 @@ package it.beije.xlsxmanager.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 
 @Slf4j
 public class XLSXManager {
 
-    public static void reader(List<String> s){
+	public static void reader(List<String> s) {
 
-        log.debug("ROW"+s.toString());
+		log.debug("ROW" + s.toString());
 
-    }
+	}
+
+	public static void main(String[] args) {
+		FileInputStream fis;
+		try {
+//			fis = new FileInputStream(new File("/Users/matteoprovezza/Desktop/xlsxManager.xlsx"));
+
+			fis = new FileInputStream(new File(
+					"/Users/matteoprovezza/Desktop/Esempio_del_file_excel_esportato_da_cassa_19_Luglio_2022.xlsx"));
+
+			XSSFWorkbook workbook = new XSSFWorkbook(fis);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			for (int i = 0; i < sheet.getLastRowNum() + 1; i++) {
+				XSSFRow row = sheet.getRow(i);
+
+				for (int j = 0; row != null && j < row.getLastCellNum(); j++) {
+
+					if (row.getCell(j) != null) {
+						String name = row.getCell(j).getCellType().name();
+						switch (name) {
+
+						case "NUMERIC": {
+							System.out.print(row.getCell(j).getNumericCellValue() + "\t\t"); // System.out.print((int)row.getCell(j).getNumericCellValue()+"\t\t");
+							break;
+						}
+
+						case "STRING": {
+							System.out.print(row.getCell(j).getStringCellValue() + "\t\t");
+							break;
+						}
+						}
+					}
+
+				}
+				System.out.println("");
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
