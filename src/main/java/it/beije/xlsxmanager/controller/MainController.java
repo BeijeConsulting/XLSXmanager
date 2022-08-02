ac48b298dd59effa1f44a1dfd484a3a2670d1280
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-08-02 11:10:57 +0200

    Init Project XLSXmanager

--- src/main/java/it/beije/xlsxmanager/controller/MainController.java
+++ src/main/java/it/beije/xlsxmanager/controller/MainController.java
@@ -5,11 +5,15 @@
 package it.beije.xlsxmanager.controller;
 
 import it.beije.xlsxmanager.service.ServiceProva;
+import it.beije.xlsxmanager.util.XLSXManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -19,9 +23,15 @@ public class MainController {
     @Autowired
     private ServiceProva serviceProva;
 
+    @Value("${xlsx.rows}")
+    private List<String> rows;
+
     @GetMapping("/h")
     public String helloWord(){
         log.debug("Prova Debug");
+
+        XLSXManager.reader(rows);
+
         return "Hello Word: "+serviceProva.salutami();
     }
 
--- src/main/java/it/beije/xlsxmanager/util/XLSXManager.java
+++ src/main/java/it/beije/xlsxmanager/util/XLSXManager.java
@@ -6,7 +6,15 @@ package it.beije.xlsxmanager.util;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Slf4j
 public class XLSXManager {
+
+    public static void reader(List<String> s){
+
+        log.debug("ROW"+s.toString());
+
+    }
 
 }
