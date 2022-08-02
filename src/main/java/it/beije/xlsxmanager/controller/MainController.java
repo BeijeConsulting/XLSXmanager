cb5421e6f8e41202f4709e6da0a18b21965ac485
Giuseppe Raddato <giuseppe.raddato.7@gmail.com>
2022-08-02 10:35:31 +0200

    Init Project XLSXmanager

--- src/main/java/it/beije/xlsxmanager/controller/MainController.java
+++ src/main/java/it/beije/xlsxmanager/controller/MainController.java
@@ -4,15 +4,18 @@
  */
 package it.beije.xlsxmanager.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MainController {
 
     @GetMapping("/h")
     public String helloWord(){
+        log.debug("Prova Debug");
         return "Hello Word";
     }
 
